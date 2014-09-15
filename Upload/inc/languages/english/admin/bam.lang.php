e96addacb4e783a444009fdccbfa63be8ef3e915
Darth-Apple <blakester95@gmail.com>
2014-09-14 23:12:23 -0400

    Initial commit

---
+++ Upload/inc/languages/english/admin/bam.lang.php
@@ -0,0 +1,103 @@
+<?php
+
+		/*    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+     */
+
+$l['bam_title'] = "Board Announcements Manager";
+$l['bam_title_acronym'] = "BAM Announcements Manager";
+$l['bam_desc'] = "Allows you to create and manage announcements in your forum header.";
+$l['bam_announcements_menu'] = "Announcements";
+$l['bam_enable'] = "Enable Board Announcements Manager?";
+$l['bam_enable_desc'] = "This setting allows you to enable or disable BAM without activating or deactivating the plugin.";
+$l['bam_random_enable'] = "Enable/Disable Random Mode";
+$l['bam_random_desc'] = "If this setting is enabled, BAM will randomly select results from a list of available announcements to display. This setting is disabled by default. ";
+$l['bam_random_max'] = "Random Results Generated";
+$l['bam_random_max_desc'] = "Maximum number of random results generated if random mode is enabled. This setting does nothing if random mode is disabled. ";
+$l['bam_random_group'] = "Random Mode Permissions";
+$l['bam_random_group_desc'] = "Configure random mode visibility by usergroup. Usergroup visibility can alternatively be configured by announcement, if preferred. ";
+$l['bam_global'] = 'Display Announcements Globally? ';
+$l['bam_global_desc'] = "This setting allows you to control the visibility of global announcements. By default, announcements are displayed only on the index page.";
+$l['bam_global_disable'] = "Disable";
+$l['bam_global_pinned'] = "Pinned Announcements Only";
+$l['bam_global_all'] = "All Announcements";
+$l['bam_index_page'] = "Index Page (Advanced Users)";
+$l['bam_index_page_desc'] = "Identify the page that will be considered the \"index page\" for non global announcements. By default, this should be set to index.php. Unless you have renamed index.php, you should not need to edit this value.  ";
+$l['bam_custom_css'] = "Custom CSS";
+$l['bam_custom_css_desc'] = "Add any custom CSS classes here. ";
+$l['bam_welcome'] = "[b]BAM has successfully been installed![/b] You may now manage your announcements via the ACP. ";
+$l['bam_date_enable'] = "Display Date Posted?";
+$l['bam_date_desc'] = "If enabled, board announcements manager will display the date posted for announcements. ";
+
+$l['add_announcement'] = 'Add Announcement';
+$l['edit_announcement'] = "Edit Announcement";
+$l['manage_announcements'] = "Manage Announcements";
+
+$l['bam_manage'] = "Manage";
+$l['bam_manage_desc'] = "This page allows you to manage, edit, delete, reorder, pin, or unpin your announcements. ";
+$l['bam_edit'] = 'Edit';
+$l['bam_edit_desc'] = "This page allows you to edit an existing announcement. ";
+$l['add_announcement_desc'] = "This page allows you to add a new announcement. ";
+$l['bam_order_success'] = "Announcement orders updated successfully. ";
+$l['bam_no_announcement'] = "Error: no announcement to update. ";
+$l['bam_pin_success'] = "Successfully pinned announcement. ";
+$l['bam_unpin_success'] = 'Successfully unpinned annoncement. ';
+$l['bam_error'] = "Error. ";
+$l['bam_form_announcement'] = 'Announcement: ';
+$l['bam_form_announcement_desc'] = 'You may use BBcode in your announcements.';
+
+$l['bam_green'] = "Green/Success";
+$l['bam_blue'] = "Blue/Notice";
+$l['bam_yellow'] = 'Yellow/Alert';
+$l['bam_red'] = "Red/Alert";
+$l['bam_custom'] = "Custom (Define Below)";
+
+$l['bam_form_style'] = "Style/Class: ";
+$l['bam_form_style_desc'] = "Select the color/class for your announcement. ";
+$l['bam_form_class_custom'] = "Custom Class: ";
+$l['bam_form_class_custom_desc'] = "Leave this field blank if you are unsure. ";
+$l['bam_form_order'] = "Display Order: ";
+$l['bam_form_order_desc'] = "Select the display order for this announcement. Leave this field blank if you are unsure. ";
+$l['bam_form_groups'] = "Group Permissions: ";
+$l['bam_form_groups_desc'] = "Select which groups will be allowed to view this announcement. Hold CTRL to select multiple groups. ";
+$l['bam_form_url'] = "Announcement URL (Optional): ";
+$l['bam_form_url_desc'] = "If you define a URL for this announcement, the announcement will be turned into a link. This setting can be left blank.";
+$l['bam_form_pinned'] = "Pin this announcement?";
+$l['bam_form_pinned_desc'] = "Do you want this announcement to be pinned by default? This setting primarily applies when BAM is running under <em>random mode</em>.";
+$l['bam_form_add_submit'] = "Add New Announcement";
+$l['bam_form_add'] = "Add New Announcement";
+$l['bam_form_edit_submit'] = "Edit Announcement";
+
+$l['bam_undefined'] = "Undefined";
+$l['bam_edit_success'] = "Successfully Edited Announcement. ";
+$l['bam_add_success'] = "Successfully Added Announcement. ";
+$l['bam_delete_success'] = "Successfully Deleted Announcement. ";
+$l['bam_delete_error'] = "Error deleting announcement: announcement not found. ";
+
+$l['bam_manage_announcement'] = "Announcement";
+$l['bam_manage_class'] = "Style/Class";
+$l['bam_manage_order'] = "Display Order";
+$l['bam_manage_actions'] = "Actions";
+$l['bam_manage_edit'] = "Edit";
+$l['bam_manage_delete'] = "Delete";
+$l['bam_manage_pin'] = "Pin";
+$l['bam_manage_unpin'] = "Unpin";
+$l['bam_manage_delete_confirm'] = "Are you sure you want to delete this announcement?";
+$l['bam_manage_actions'] = "Actions";
+$l['bam_manage_null'] = "No announcements found. ";
+$l['bam_manage_order_submit'] = "Update Display Orders";
+$l['bam_invalid_post_code'] = "Invalid post code detected. Please try again. ";
+
+
+
